ca382243741eb220302b30297e3ca23201f9fd99
Mhyran <maxime.rffd@gmail.com>
2020-04-23 10:43:15 +0200

    Authenticator & security.yaml configurations, add ProjectController

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    /**
+     * @Route("/admin/project", name="project_list")
+     */
+    public function index()
+    {
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+}
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -87,7 +87,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('project_list'));
     }
 
     protected function getLoginUrl()
